50c09a926e523b181b26f728ef3dd6864afca15b
Anand Dubey(Aadi) <115421478+ananddubey1999@users.noreply.github.com>
2023-01-25 22:49:04 +0530

    Update CounterList.js

--- src/components/CounterList.js
+++ src/components/CounterList.js
@@ -1,25 +1,21 @@
-import React, { useContext } from "react";
-import { counterContext } from "../counterContext";
-import { Counter } from "./Counter";
+import React,{useContext} from 'react';
+import { counterContext } from '../counterContext';
+import { Counter } from './Counter';
 
-export const CounterList = () => {
-  const { count, setCount } = useContext(counterContext);
+export const CounterList = () =>{
 
-  const onClick = () => {
-    setCount(count + 1);
-  };
+    const {count,setCount} = useContext(counterContext)
 
-  return (
-    <ol>
-      <li>
-        <Counter key={1} id="1" value={count} onClick={onClick} />
-      </li>
-      <li>
-        <Counter key={2} id="2" value={count} onClick={onClick} />
-      </li>
-      <li>
-        <Counter key={3} id="3" value={count} onClick={onClick} />
-      </li>
-    </ol>
-  );
-};
+    const onClick = () =>{
+        setCount(count+1)
+    }
+
+    return(
+        <ol>
+            <li><Counter key={1} id="1" value={count} onClick={onClick}/></li>
+            <li><Counter key={2} id="2" value={count} onClick={onClick}/></li>
+            <li><Counter key={3} id="3" value={count} onClick={onClick}/></li>
+        </ol>
+    )
+
+} 
